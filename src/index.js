b1f35339a8806d997e5e9d0417109874011f4b94
EduOnar <eduardoonar@gmail.com>
2024-08-15 13:14:06 -0400

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import {saludar} from './js/componentes.js';
 import './style.css';
 
-const nombre = 'Eduardo';
-saludar(nombre);
+import {Todo, TodoList} from './classes'
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo)); // forma mas abreviado => {todoList.todos.forEach(crearTodoHtml)}
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,11 +1,101 @@
+import { Todo } from "../classes";
+import { todoList } from '../index';
 
-import '../css/componentes.css';
+//referencias en el html
+const divTodoList   = document.querySelector('.todo-list');
+const txtInput      = document.querySelector('.new-todo');
+const btnBorrar     = document.querySelector('.clear-completed');
+const ulFilters     = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
+const contador      = document.querySelector('strong');
 
-export const saludar = (nombre) => {
-    console.log('Creando etiqueta h1 en el HTML');
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre} que tal`;
 
-    document.body.append(h1);
+export const crearTodoHtml = (todo) => {
+    const htmlTodo = `
+    <li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
+		<div class="view">
+			<input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
+			<label>${ todo.tarea }</label>
+			<button class="destroy"></button>
+		</div>
+		<input class="edit" value="Create a TodoMVC template">
+	</li>   `;
+
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+
+    divTodoList.append(div.firstElementChild);
+    return div.firstElementChild;
 }
+
+const contadorPendientes = () => {
+    contador.innerText = todoList.todos.filter( todo => todo.completado == false).length;
+}
+
+//eventos
+txtInput.addEventListener( 'keyup', (event)=>{
+    if (event.keyCode === 13 && txtInput.value.length > 0) {
+        const nuevoTodo = new Todo( txtInput.value );
+        todoList.nuevoTodo( nuevoTodo );
+
+        crearTodoHtml(nuevoTodo);
+        txtInput.value = '';
+        contadorPendientes();
+    }
+});
+
+divTodoList.addEventListener('click', (event) => {
+    const nombreElemento = event.target.localName;
+    const todoElement    = event.target.parentElement.parentElement;
+    const todoId         = todoElement.getAttribute('data-id');
+
+    if (nombreElemento.includes('input')) {
+        todoList.marcarCompletado(todoId);
+        todoElement.classList.toggle('completed');
+    } else if (nombreElemento.includes('button')) {
+        todoList.eliminarTodo(todoId);
+        divTodoList.removeChild(todoElement);
+    }
+
+    contadorPendientes();
+});
+
+btnBorrar.addEventListener('click', () => {
+    todoList.eliminarCompletados();
+    for (let i = divTodoList.children.length - 1; i >= 0; i--) {
+        const elem = divTodoList.children[i];
+        if (elem.classList.contains('completed')) {
+            divTodoList.removeChild(elem);
+        }
+    }
+});
+
+ulFilters.addEventListener('click', (event) => {
+    const filtro = event.target.text;
+
+    if ( !filtro ) { return; }
+
+    anchorFiltros.forEach(elemento => elemento.classList.remove('selected'))
+    event.target.classList.add('selected');
+
+    for (const elemento of divTodoList.children) {
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch ( filtro) {
+
+            case 'Pendientes':
+                if (completado) {
+                    elemento.classList.add('hidden');
+                }
+            break;
+
+            case 'Completados':
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                }
+            break;
+        }
+    }
+});
